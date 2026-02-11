cfaf35fb575e526a9aea5154dadc36efec5559fd
kiriklzz <kirill.zabrodin03@gmail.com>
2026-02-11 13:18:11 +0300

    bug fix on forgot password page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def switch_locale_url(locale)
+    path = request.path
+
+    if path.match?(%r{\A/(ru|en)/users/password\z})
+      return "/#{locale}/users/password/new"
+    end
+
+    if path.match?(%r{\A/(ru|en)/users\z})
+      return "/#{locale}/users/sign_up"
+    end
+
+    if path.match?(%r{\A/(ru|en)(/|$)})
+      path.sub(%r{\A/(ru|en)}, "/#{locale}")
+    else
+      "/#{locale}#{path}"
+    end
+  end
 end
